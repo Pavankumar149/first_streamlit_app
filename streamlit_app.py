1f8cc91bffd0f225b60ff8c0c0fc27802227df59
Pavankumar Dasari <94543553+Pavankumar149@users.noreply.github.com>
2023-02-24 11:20:41 +0530

    Update streamlit_app.py
    made changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healty Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & banna ')
+streamlit.title('Hello Shiva sai ')
+streamlit.header('Your Pc is got strucked ')
+streamlit.text('You want to take some fruits.... ')
 streamlit.text('this is sample text for streamlit')
 streamlit.text('we have been trying to use streamlit')
