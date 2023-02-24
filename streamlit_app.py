2c7621f90172177a028c53237e50d09e873a24a0
Pavankumar Dasari <94543553+Pavankumar149@users.noreply.github.com>
2023-02-24 11:18:50 +0530

    Update streamlit_app.py
    added menu as well as streamlit functions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & banna ')
+streamlit.text('this is sample text for streamlit')
+streamlit.text('we have been trying to use streamlit')
