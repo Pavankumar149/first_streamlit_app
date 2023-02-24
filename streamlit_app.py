6972a7eb581cc330ed221ea1f03907d9379873e4
Pavankumar Dasari <94543553+Pavankumar149@users.noreply.github.com>
2023-02-24 11:22:08 +0530

    Update streamlit_app.py
    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('Your Pc is got strucked ')
 streamlit.text('You want to take some fruits.... ')
 streamlit.text('this is sample text for streamlit')
 streamlit.text('we have been trying to use streamlit')
+
+steamlit.text('Hey shiva sai')
